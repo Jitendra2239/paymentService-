3e7e856bb2df58e2c5490b48b2a53d1273abe93d
Jitendra Kumar <kjitendra9080@gmail.com>
2026-03-15 13:14:53 +0530

    distributed transaction implemented using saga pattren using kafka message queue

--- src/main/java/com/jitendra/paymentservice/dto/PaymentResponse.java
+++ src/main/java/com/jitendra/paymentservice/dto/PaymentResponse.java
@@ -1,10 +1,13 @@
 package com.jitendra.paymentservice.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
-
+import lombok.NoArgsConstructor;
+@Builder
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class PaymentResponse {
 
     private String paymentId;
--- src/main/java/com/jitendra/paymentservice/service/PaymentServiceImpl.java
+++ src/main/java/com/jitendra/paymentservice/service/PaymentServiceImpl.java
@@ -1,27 +1,30 @@
 package com.jitendra.paymentservice.service;
 
+import com.jitendra.event.InventoryReservedEvent;
+import com.jitendra.event.PaymentFailedEvent;
+import com.jitendra.event.PaymentSuccessEvent;
 import com.jitendra.paymentservice.config.PaymentGatewayClient;
 import com.jitendra.paymentservice.dto.GatewayResponse;
 import com.jitendra.paymentservice.dto.PaymentRequest;
 import com.jitendra.paymentservice.dto.PaymentResponse;
+
 import com.jitendra.paymentservice.model.Payment;
 import com.jitendra.paymentservice.repository.PaymentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
-
+@RequiredArgsConstructor
 @Service
 public class PaymentServiceImpl implements PaymentService {
 
     private final PaymentRepository paymentRepository;
     private final PaymentGatewayClient gatewayClient;
+    private final KafkaTemplate<String,Object> kafkaTemplate;
 
-    public PaymentServiceImpl(PaymentRepository paymentRepository,
-                              PaymentGatewayClient gatewayClient) {
-        this.paymentRepository = paymentRepository;
-        this.gatewayClient = gatewayClient;
-    }
 
     @Override
     public PaymentResponse processPayment(PaymentRequest request) {
@@ -81,4 +84,37 @@ public class PaymentServiceImpl implements PaymentService {
                 "Payment status fetched"
         );
     }
+    @KafkaListener(topics = "inventory-reserved", groupId = "payment-group")
+    public void consumeInventoryReserved(InventoryReservedEvent event) {
+
+        System.out.println("Processing payment for Order: " + event.getOrderId());
+        PaymentRequest request =new PaymentRequest();
+        PaymentResponse paymentResponse  = new PaymentResponse();
+      paymentResponse.setStatus("FAILED");
+      paymentResponse.setPaymentId("payment-id");
+      paymentResponse.setMessage("message");
+        if( paymentResponse.getStatus().equals("SUCCESS") ) {
+            PaymentSuccessEvent event1 =new PaymentSuccessEvent();
+            event1.setOrderId(event.getOrderId());
+            event1.setPaymentId(paymentResponse.getPaymentId());
+
+            kafkaTemplate.send("payment-success", event1);
+        } else {
+            PaymentFailedEvent event1 =new PaymentFailedEvent();
+            event1.setOrderId(event.getOrderId());
+            event1.setPaymentId(paymentResponse.getPaymentId());
+            kafkaTemplate.send("payment-failed", event1);
+        }
+    }
+//    @KafkaListener(topics = "order-created")
+//    public void processPayment(OrderCreatedEvent event){
+//
+//        System.out.println("Processing payment");
+//
+//        PaymentSuccessEvent paymentEvent =
+//                new PaymentSuccessEvent(event.getOrderId(),"PAY123");
+//
+//        kafkaTemplate.send("payment-success", paymentEvent);
+//
+//    }
 }
